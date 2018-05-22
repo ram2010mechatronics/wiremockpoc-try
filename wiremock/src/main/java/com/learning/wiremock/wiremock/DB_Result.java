a247a12ea3dc78b81dd2efae140840567899bdbe
ram2010mechatronics <ram2010mechatronics@gmail.com>
2018-05-22 13:04:34 +0530

    in progress

--- wiremock/src/main/java/com/learning/wiremock/wiremock/DB_Result.java
+++ wiremock/src/main/java/com/learning/wiremock/wiremock/DB_Result.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
@@ -13,30 +12,27 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class DB_Result {
 
+    private static String csvPath = "D:\\WireMock\\wire-mock-master\\wiremockpoc-try\\wiremock\\src\\test\\resources\\csv";
+    private static int rows = 1;
+
     public static void main(String args[]) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.newDocument();
-        Element results = doc.createElement("Results");
+        Element results = doc.createElement("empAddress");
         doc.appendChild(results);
-
-        Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-        Connection con = DriverManager
-                .getConnection("jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=c:/access.mdb");
-
-        ResultSet rs = con.createStatement().executeQuery("select * from product");
-
+        Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        Connection con = DriverManager.getConnection("jdbc:hsqldb:file:" + csvPath, "CSV", "");
+        ResultSet rs = con.createStatement().executeQuery("select * from emp_address where \"emp_no\" = '7369'");
         ResultSetMetaData rsmd = rs.getMetaData();
         int colCount = rsmd.getColumnCount();
-
         while (rs.next()) {
-            Element row = doc.createElement("Row");
+            Element row = doc.createElement("AddressLine"+rows);
             results.appendChild(row);
             for (int i = 1; i <= colCount; i++) {
                 String columnName = rsmd.getColumnName(i);
@@ -45,6 +41,7 @@ public class DB_Result {
                 node.appendChild(doc.createTextNode(value.toString()));
                 row.appendChild(node);
             }
+            rows = rows + 1;
         }
         DOMSource domSource = new DOMSource(doc);
         TransformerFactory tf = TransformerFactory.newInstance();
@@ -55,9 +52,7 @@ public class DB_Result {
         StringWriter sw = new StringWriter();
         StreamResult sr = new StreamResult(sw);
         transformer.transform(domSource, sr);
-
         System.out.println(sw.toString());
-
         con.close();
         rs.close();
     }
